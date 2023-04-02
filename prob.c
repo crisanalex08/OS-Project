c49e7db8b485f4e293a47d5d9997a326f84ebd9d
Alex Crisan <alcat@Alex>
2023-04-02 22:50:02 +0300

    Finished the first problem

--- prob.c
+++ prob.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
 void printRegularFileMenu()
 {
@@ -14,7 +15,6 @@ void printRegularFileMenu()
     printf("-a : Access rights\n");
     printf("-l : Symbolic Link | give link name \n");
     printf("-e : Exit\n");
-
 }
 
 void printSymbolicLinkMenu()
@@ -22,7 +22,6 @@ void printSymbolicLinkMenu()
     printf("-n : Link name\n");
     printf("-l : Delete link\n");
     printf("-d : Size of the link\n");
-    printf("-l : Symbolic Link | give link name \n");
     printf("-a : Access rights\n");
     printf("-e : Exit\n");
 }
@@ -30,7 +29,9 @@ void printSymbolicLinkMenu()
 int main(int arg, char* argv[])
 {
     struct stat sb;
-    char choice[2];
+    int badChoice = 0;
+    int regFile = 0;
+    char choice[10];
     if(arg < 2)
     {
         printf("Usage:<filename> path1, path2, ...");
@@ -44,6 +45,7 @@ int main(int arg, char* argv[])
 
     if(S_ISREG(sb.st_mode))
     {
+        regFile = 1;
         printf("Regular File Menu\n");
         printRegularFileMenu();
     }
@@ -57,23 +59,86 @@ int main(int arg, char* argv[])
         printf("Not a regular file or symbolic link\n");
     }
     
-    
-    while (strcmp(choice, "-e"))
-    {
-        printf("Enter your choice: ");
-        scanf("%s", &choice);
-
-        switch (choice[1])
+    if(regFile){
+        while (strcmp(choice, "-e") || strcmp(choice, "e") || badChoice)
         {
-        case 'n':
-            printf("%s", argv[1]);
-            break;
-        
-        default:
-            break;
+            printf("Enter your choice: ");
+            scanf("%s", &choice);
+
+            for(int i = 1; i < strlen(choice); i++){
+                switch (choice[i])
+                {
+                    case 'n':
+                        printf("File Name: %s\n", argv[1]);
+                        break;
+                    case 'd':
+                        printf("File Size: %ld\n", sb.st_size);
+                        break;
+                    case 'h':
+                        printf("Number of Hard Links: %ld\n", sb.st_nlink);
+                        break;
+                    case 'm':
+                        printf("Time of last modification: %s\n", ctime(&(sb.st_mtime)));
+                        break;
+                    case 'a':
+                        printf("Acces rights: %o\n", sb.st_mode);//
+                        break;
+                    case 'l':
+                        char slinkName[100];
+                        printf("Enter the symbolic link name: ");
+                        scanf("%s", &slinkName);
+                        if (symlink(argv[1], slinkName) == -1) {
+                            perror("symlink");
+                            exit(EXIT_FAILURE);
+                        }
+                    case 'e':
+                        printf("Exiting...\n");
+                        exit(EXIT_SUCCESS);
+                        break;
+                    default:
+                        printf("Invalid choice: \"%c\"\n", choice[i]);
+                        badChoice = 1;
+                        break;
+                }
+            }
+        }
+    }else{
+         while (strcmp(choice, "-e") || strcmp(choice, "e") || badChoice)
+        {
+            printf("Enter your choice: ");
+            scanf("%s", &choice);
+
+            for(int i = 1; i < strlen(choice); i++){
+                switch (choice[i])
+                {
+                    case 'n':
+                        printf("Link Name: %s\n", argv[1]);
+                        break;
+                    case 'd':
+                        printf("Link Size: %ld\n", sb.st_size);
+                        break;
+                    case 'l':
+                        printf("Deleting link\n");
+                        if (unlink(argv[1]) == -1) {
+                            perror("unlink");
+                            exit(EXIT_FAILURE);
+                        }
+                        printf("Link deleted\n");
+                        break;
+                    case 'a':
+                        printf("Acces rights: %o\n", sb.st_mode);//
+                        break;
+                    case 'e':
+                        printf("Exiting...\n");
+                        exit(EXIT_SUCCESS);
+                        break;
+                    default:
+                        printf("Invalid choice: \"%c\"\n", choice[i]);
+                        badChoice = 1;
+                        break;
+                }
+            }
         }
     }
-    
-    
     return 0;
 }
